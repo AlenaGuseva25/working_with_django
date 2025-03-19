3feef94b9f8b7179461f8ac1d6bacb4460bfb047
Alena <alittlekindness94@gmail.com>
2025-03-19 17:42:20 +0500

    upd2

--- custom_users/models.py
+++ custom_users/models.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    username = models.CharField(max_length=150)
     email = models.EmailField(unique=True, verbose_name='Email')
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name='Аватар')
     phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='Номер телефона')
     country = models.CharField(max_length=50, null=True, blank=True, verbose_name='Страна')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.email
